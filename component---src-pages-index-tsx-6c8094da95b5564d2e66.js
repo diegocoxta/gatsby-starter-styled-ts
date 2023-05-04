e07e2da6adc754c925497e60c280a573ab7dfca3
diegocoxta <diegocoxta@users.noreply.github.com>
2023-05-04 22:08:57 +0000

    Automated deployment: Thu May  4 22:08:57 UTC 2023 505d20b56ca40cfe343c5aa9351bb61aff281a5b

---
+++ component---src-pages-index-tsx-6c8094da95b5564d2e66.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_styled_ts=self.webpackChunkgatsby_starter_styled_ts||[]).push([[691],{7200:function(t,e,s){s.r(e),s.d(e,{default:function(){return r}});var a=s(7294);const n=s(2788).default.div.withConfig({displayName:"pages__Paragraph",componentId:"sc-1tpjs49-0"})(["color:#5e3993;"]);function r(){return a.createElement(n,null,"Hello World")}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-6c8094da95b5564d2e66.js.map
